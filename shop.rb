87bc7a00d98e6ae4b0c43501d2da6855c99af1cf
ciyuxu <YOUR@EMAIL.com>
2017-08-30 17:38:46 +0200

    Defined choosing product

--- shop.rb
+++ shop.rb
@@ -11,12 +11,12 @@ end
 # defined print_divider and print_progress_bar
 
 @shopping_cart = []
-@product = [{reference_no: 01, name: "primer", price: 10},
-{reference_no: 02, name: "foundation", price: 20},
-{reference_no: 03, name: "eyeshadow", price: 15},
-{reference_no: 04, name: "mascara", price: 15},
-{reference_no: 05, name: "lipstick", price: 10},
-{reference_no: 06, name: "blush", price: 10},
+@product = [{reference_no: 1, name: "primer", price: 10},
+{reference_no: 2, name: "foundation", price: 20},
+{reference_no: 3, name: "eyeshadow", price: 15},
+{reference_no: 4, name: "mascara", price: 15},
+{reference_no: 5, name: "lipstick", price: 10},
+{reference_no: 6, name: "blush", price: 10},
 ]
 
 #defined shopping_cart and product lipstick
@@ -29,3 +29,12 @@ puts "We have: "
 end
 
 print_products
+
+#defined product line print out
+
+def choosing_product
+  puts "Please type-in product by reference number:"
+  chosen_reference_number = gets.to_i
+end
+
+#defined choosing_product
