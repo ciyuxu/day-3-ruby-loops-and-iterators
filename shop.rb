1096ab2e2d9fcbf1d292352975baef7ce44357d2
ciyuxu <YOUR@EMAIL.com>
2017-08-30 21:08:33 +0200

    Defined pikcing product from ref number

--- shop.rb
+++ shop.rb
@@ -11,7 +11,7 @@ end
 # defined print_divider and print_progress_bar
 
 @shopping_cart = []
-@product = [{reference_no: 1, name: "primer", price: 10},
+@products = [{reference_no: 1, name: "primer", price: 10},
 {reference_no: 2, name: "foundation", price: 20},
 {reference_no: 3, name: "eyeshadow", price: 15},
 {reference_no: 4, name: "mascara", price: 15},
@@ -23,7 +23,7 @@ end
 
 def print_products
 puts "We have: "
-  @product.each do |product_name|
+  @products.each do |product_name|
     puts " #{product_name[:name]}"
   end
 end
@@ -32,9 +32,26 @@ print_products
 
 #defined product line print out
 
-def choosing_product
+def choosing_product_by_ref_number
   puts "Please type-in product by reference number:"
-  chosen_reference_number = gets.to_i
+  gets.to_i
 end
 
+
+
 #defined choosing_product
+
+
+
+
+
+def pickingout_product_by_ref_number(ref_number)
+  @products.each do |product|
+    if product[:reference_no]== ref_number
+      return product
+    end
+  end
+end
+puts pickingout_product_by_ref_number(choosing_product_by_ref_number)
+
+#defined picking product from ref number
